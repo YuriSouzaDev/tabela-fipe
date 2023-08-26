77aa4089c3fe028819b5d7be10a42263a2fe9497
YuriSouzaDev <yurisouza.developer@gmail.com>
2023-08-26 09:42:06 -0300

    :recycle: refactor: reutilizacao de codigo e separacao de responsabilidade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,65 @@
 const inType = document.querySelector("#inType");
 const inMarca = document.querySelector("#inMarca");
 const inModel = document.querySelector("#inModel");
+const inYear = document.querySelector("#inYear");
 
-async function fetchType() {
-  const urlType = `https://parallelum.com.br/fipe/api/v1/${inType.value}/marcas`;
+// function para retulizar nos inputs
+function createOption(value, text) {
+  const option = document.createElement("option");
+  option.setAttribute("value", value);
+  option.innerText = text;
+  return option;
+}
 
+// fecth para reutlizar dentro das funcoes de criacao dos html dos inputs
+async function fetchJSON(url) {
   try {
-    const typeResponse = await fetch(urlType);
-    const marcaJSON = await typeResponse.json();
-
-    inMarca.innerHTML = "";
-    inModel.innerHTML = "";
-
-    marcaJSON.forEach((marca) => {
-      const optionMarca = document.createElement("option");
-      optionMarca.setAttribute("value", marca.codigo);
-      optionMarca.innerText = marca.nome;
-      inMarca.appendChild(optionMarca);
-    });
-  } catch (erro) {
-    console.log(erro);
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    console.log(error);
   }
 }
 
-function createMarcaOption(marca) {
-  const optionMarca = document.createElement("option");
-  optionMarca.setAttribute("value", marca.codigo);
-  optionMarca.innerText = marca.nome;
-  return optionMarca;
+// funcao que retorna as marcas se baseando no tipo de veiculo
+async function fetchType() {
+  inType.innerHTML = `<select name="inType" id="inType">
+  <option value="carros">Carros</option>
+  <option value="motos">Motos</option>
+  <option value="caminhoes">Caminhões</option>
+  </select>`;
+
+  const urlType = `https://parallelum.com.br/fipe/api/v1/${inType.value}/marcas`;
+
+  const marcaJSON = await fetchJSON(urlType);
+
+  inMarca.innerHTML = "";
+
+  if (marcaJSON) {
+    marcaJSON.forEach((marca) => {
+      const optionMarca = createOption(marca.codigo, marca.nome);
+      inMarca.appendChild(optionMarca);
+    });
+  }
 }
 
-inType.addEventListener("change", fetchType);
-
-// CRIAR APARECIMENTO DE MODELO DO CARRO
+// funcao que retorna o modelo do carro se baseando na marca
 async function fetchModel() {
   const urlModel = `https://parallelum.com.br/fipe/api/v1/${inType.value}/marcas/${inMarca.value}/modelos`;
 
-  try {
-    const marcaResponse = await fetch(urlModel);
-    const modelJSON = await marcaResponse.json();
+  const modelJSON = await fetchJSON(urlModel);
 
-    inModel.innerHTML = "";
+  inModel.innerHTML = "";
 
+  if (inModel && modelJSON.modelos) {
     modelJSON.modelos.forEach((model) => {
-      const optionModel = createModelOption(model);
-      inModel.appendChild(optionModel);
+      const optionMarca = createOption(model.codigo, model.nome);
+      inModel.appendChild(optionMarca);
     });
-  } catch (erro) {
-    console.log(erro);
   }
 }
 
-function createModelOption(model) {
-  const optionModel = document.createElement("option");
-  optionModel.setAttribute("value", model.codigo);
-  optionModel.innerText = model.nome;
-  return optionModel;
-}
-
-inMarca.addEventListener("change", fetchModel);
-
 // CRIAR APARECIMENTO DE ANO DO CARRO
+
+inType.addEventListener("change", fetchType);
+inMarca.addEventListener("change", fetchModel);
